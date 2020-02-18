84ecb4ceb169e0613a25b97b171fd8d9cffed093
Francesco De Carlo <decarlof@gmail.com>
2020-02-18 11:50:59 -0600

    now logs does not save colors in the log file

--- globus/log.py
+++ globus/log.py
@@ -4,53 +4,44 @@
 '''
 import logging
 
-# Logging defines
-__GREEN = "\033[92m"
-__RED = '\033[91m'
-__YELLOW = '\033[33m'
-__ENDC = '\033[0m'
-
-
-logger = None
-info_extra={'endColor': __ENDC, 'color': __GREEN}
-warn_extra={'endColor': __ENDC, 'color': __YELLOW}
-error_extra={'endColor': __ENDC, 'color': __RED}
+logger = logging.getLogger(__name__)
 
 def info(msg):
-    global logger
-    global info_extra
-    logger.info(msg, extra=info_extra)
+    logger.info(msg)
 
 def error(msg):
-    global logger
-    global error_extra
-    logger.error(msg, extra=error_extra)
+    logger.error(msg)
 
 def warning(msg):
-    global logger
-    global warn_extra
-    logger.warning(msg, extra=warn_extra)
+    logger.warning(msg)
 
-
-def setup_logger(log_name, stream_to_console=True):
-    global logger
-    global info_extra
-    global warn_extra
-    global error_extra
-
-    info_extra['logger_name'] = log_name
-    warn_extra['logger_name'] = log_name
-    error_extra['logger_name'] = log_name
-    logger = logging.getLogger(log_name)
-    fHandler = logging.FileHandler(log_name)
+def setup_custom_logger(lfname, stream_to_console=True):
+    fHandler = logging.FileHandler(lfname)
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s %(color)s  %(message)s %(endColor)s")
-    fHandler.setFormatter(formatter)
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+    fHandler.setFormatter(file_formatter)
     logger.addHandler(fHandler)
     if stream_to_console:
         ch = logging.StreamHandler()
-        ch.setFormatter(formatter)
+        ch.setFormatter(ColoredLogFormatter('%(asctime)s - %(message)s'))
         ch.setLevel(logging.DEBUG)
         logger.addHandler(ch)
 
-
+class ColoredLogFormatter(logging.Formatter):
+    def __init__(self, fmt, datefmt=None, style='%'):
+        # Logging defines
+        self.__GREEN = "\033[92m"
+        self.__RED = '\033[91m'
+        self.__YELLOW = '\033[33m'
+        self.__ENDC = '\033[0m'
+        super().__init__(fmt, datefmt, style)
+    
+    
+    def formatMessage(self,record):
+        if record.levelname=='INFO':
+            record.message = self.__GREEN + record.message + self.__ENDC
+        elif record.levelname == 'WARNING':
+            record.message = self.__YELLOW + record.message + self.__ENDC
+        elif record.levelname == 'ERROR':
+            record.message = self.__RED + record.message + self.__ENDC
+        return super().formatMessage(record)
