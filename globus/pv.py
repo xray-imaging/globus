c31d01dcd7d596ac271fedb885e7de3716af607f
Francesco De Carlo <decarlof@gmail.com>
2020-11-06 19:45:25 -0600

    setting as_string=True for string PV reading

--- globus/pv.py
+++ globus/pv.py
@@ -25,27 +25,11 @@ def update_experiment_info(args):
     '''
     global_PVs = init_general_PVs(args)
 
-    year_month = global_PVs['ExperimentYearMonth'].get()
-    pi_last_name = global_PVs['UserLastName'].get()
-    pi_email = global_PVs['UserEmail'].get()
-    GUP_number = global_PVs['GUP#'].get()   
-    GUP_title = global_PVs['ProposalTitle'].get()
+    year_month = global_PVs['ExperimentYearMonth'].get(as_string=True)
+    pi_last_name = global_PVs['UserLastName'].get(as_string=True)
+    pi_email = global_PVs['UserEmail'].get(as_string=True)
+    gup_number = global_PVs['GUP#'].get(as_string=True)   
+    gup_title = global_PVs['ProposalTitle'].get(as_string=True)
 
-    # convert list of chars into string and remove NULL termination
-    year_month_str = string_from_pv_output(year_month)
-    pi_last_name_str = string_from_pv_output(pi_last_name)
-    pi_email_str = string_from_pv_output(pi_email)
-    gup_number_str = string_from_pv_output(GUP_number)
-    gup_title_str = string_from_pv_output(GUP_title)
-
-    return year_month_str, pi_last_name_str, gup_number_str, gup_title_str
-
-
-def string_from_pv_output(pv_output_in):
-    '''Converts pv_output_in to string if necessary.
-    '''
-    if type(pv_output_in) == str:
-        return pv_output_in
-    elif type(pv_output_in) == np.ndarray:
-        return "".join([chr(c) for c in pv_output_in]).rstrip('\x00')
+    return year_month, pi_last_name, gup_number, gup_title
  
