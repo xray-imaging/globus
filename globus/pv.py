2aace04b444f5806ace48117aba6cadfbc0057cc
Francesco De Carlo <decarlof@gmail.com>
2020-11-11 18:17:58 -0600

    standardized config label names

--- globus/pv.py
+++ globus/pv.py
@@ -11,8 +11,8 @@ def init_general_PVs(args):
     global_PVs['ExperimentYearMonth'] = PV(prefix + scan_prefix + args.experiment_year_month)
     global_PVs['UserEmail'] = PV(prefix + scan_prefix + args.user_email)
     global_PVs['UserLastName'] = PV(prefix + scan_prefix + args.user_last_name)
-    global_PVs['GUP#'] = PV(prefix + scan_prefix + args.GUP_number)
-    global_PVs['ProposalTitle'] = PV(prefix + scan_prefix + args.GUP_desc)
+    global_PVs['ProposalNumber'] = PV(prefix + scan_prefix + args.proposal_number)
+    global_PVs['ProposalTitle'] = PV(prefix + scan_prefix + args.proposal_title)
 
     return global_PVs
 
@@ -28,7 +28,7 @@ def update_experiment_info(args):
     year_month = global_PVs['ExperimentYearMonth'].get(as_string=True)
     pi_last_name = global_PVs['UserLastName'].get(as_string=True)
     pi_email = global_PVs['UserEmail'].get(as_string=True)
-    gup_number = global_PVs['GUP#'].get(as_string=True)   
+    gup_number = global_PVs['ProposalNumber'].get(as_string=True)   
     gup_title = global_PVs['ProposalTitle'].get(as_string=True)
 
     return year_month, pi_last_name, gup_number, gup_title
